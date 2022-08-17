9554ea371a4eaefdd2bb4e2c4d0088d75f8ceae0
Sergey Petrinich <petrinichshadow@gmail.com>
2022-08-17 18:23:50 +0300

    footer v1.1 \ header v1.1 \ cleared files

--- src/js/files/functions.js
+++ src/js/files/functions.js
@@ -39,11 +39,7 @@ export let isMobile = {
         return isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows();
     },
 };
-/* Добавление класса touch для HTML если браузер мобильный */
-export function addTouchClass() {
-    // Добавление класса _touch для HTML если браузер мобильный
-    if (isMobile.any()) document.documentElement.classList.add("touch");
-}
+
 // Добавление loaded для HTML после полной загрузки страницы
 export function addLoadedClass() {
     window.addEventListener("load", function () {
@@ -62,18 +58,6 @@ export function getHash() {
 export function setHash(hash) {
     hash = hash ? `#${hash}` : window.location.href.split("#")[0];
     history.pushState("", "", hash);
-}
-// Учет плавающей панели на мобильных устройствах при 100vh
-export function fullVHfix() {
-    const fullScreens = document.querySelectorAll("[data-fullscreen]");
-    if (fullScreens.length && isMobile.any()) {
-        window.addEventListener("resize", fixHeight);
-        function fixHeight() {
-            let vh = window.innerHeight * 0.01;
-            document.documentElement.style.setProperty("--vh", `${vh}px`);
-        }
-        fixHeight();
-    }
 }
 // Вспомогательные модули плавного расскрытия и закрытия объекта ======================================================================================================================================================================
 export let _slideUp = (target, duration = 500, showmore = 0) => {
@@ -471,123 +455,6 @@ export function menuClose() {
     bodyUnlock();
     document.documentElement.classList.remove("menu-open");
 }
-// Модуль "показать еще" =======================================================================================================================================================================================================================
-/*
-Документация по работе в шаблоне: https://template.fls.guru/template-docs/modul-pokazat-eshhjo.html
-Сниппет (HTML): showmore
-*/
-export function showMore() {
-    window.addEventListener("load", function (e) {
-        const showMoreBlocks = document.querySelectorAll("[data-showmore]");
-        let showMoreBlocksRegular;
-        let mdQueriesArray;
-        if (showMoreBlocks.length) {
-            // Получение обычных объектов
-            showMoreBlocksRegular = Array.from(showMoreBlocks).filter(function (item, index, self) {
-                return !item.dataset.showmoreMedia;
-            });
-            // Инициализация обычных объектов
-            showMoreBlocksRegular.length ? initItems(showMoreBlocksRegular) : null;
-
-            document.addEventListener("click", showMoreActions);
-            window.addEventListener("resize", showMoreActions);
-
-            // Получение объектов с медиа запросами
-            mdQueriesArray = dataMediaQueries(showMoreBlocks, "showmoreMedia");
-            if (mdQueriesArray && mdQueriesArray.length) {
-                mdQueriesArray.forEach((mdQueriesItem) => {
-                    // Событие
-                    mdQueriesItem.matchMedia.addEventListener("change", function () {
-                        initItems(mdQueriesItem.itemsArray, mdQueriesItem.matchMedia);
-                    });
-                });
-                initItemsMedia(mdQueriesArray);
-            }
-        }
-        function initItemsMedia(mdQueriesArray) {
-            mdQueriesArray.forEach((mdQueriesItem) => {
-                initItems(mdQueriesItem.itemsArray, mdQueriesItem.matchMedia);
-            });
-        }
-        function initItems(showMoreBlocks, matchMedia) {
-            showMoreBlocks.forEach((showMoreBlock) => {
-                initItem(showMoreBlock, matchMedia);
-            });
-        }
-        function initItem(showMoreBlock, matchMedia = false) {
-            showMoreBlock = matchMedia ? showMoreBlock.item : showMoreBlock;
-            let showMoreContent = showMoreBlock.querySelectorAll("[data-showmore-content]");
-            let showMoreButton = showMoreBlock.querySelectorAll("[data-showmore-button]");
-            showMoreContent = Array.from(showMoreContent).filter((item) => item.closest("[data-showmore]") === showMoreBlock)[0];
-            showMoreButton = Array.from(showMoreButton).filter((item) => item.closest("[data-showmore]") === showMoreBlock)[0];
-            const hiddenHeight = getHeight(showMoreBlock, showMoreContent);
-            if (matchMedia.matches || !matchMedia) {
-                if (hiddenHeight < getOriginalHeight(showMoreContent)) {
-                    _slideUp(showMoreContent, 0, hiddenHeight);
-                    showMoreButton.hidden = false;
-                } else {
-                    _slideDown(showMoreContent, 0, hiddenHeight);
-                    showMoreButton.hidden = true;
-                }
-            } else {
-                _slideDown(showMoreContent, 0, hiddenHeight);
-                showMoreButton.hidden = true;
-            }
-        }
-        function getHeight(showMoreBlock, showMoreContent) {
-            let hiddenHeight = 0;
-            const showMoreType = showMoreBlock.dataset.showmore ? showMoreBlock.dataset.showmore : "size";
-            if (showMoreType === "items") {
-                const showMoreTypeValue = showMoreContent.dataset.showmoreContent ? showMoreContent.dataset.showmoreContent : 3;
-                const showMoreItems = showMoreContent.children;
-                for (let index = 1; index < showMoreItems.length; index++) {
-                    const showMoreItem = showMoreItems[index - 1];
-                    hiddenHeight += showMoreItem.offsetHeight;
-                    if (index == showMoreTypeValue) break;
-                }
-            } else {
-                const showMoreTypeValue = showMoreContent.dataset.showmoreContent ? showMoreContent.dataset.showmoreContent : 150;
-                hiddenHeight = showMoreTypeValue;
-            }
-            return hiddenHeight;
-        }
-        function getOriginalHeight(showMoreContent) {
-            let parentHidden;
-            let hiddenHeight = showMoreContent.offsetHeight;
-            showMoreContent.style.removeProperty("height");
-            if (showMoreContent.closest(`[hidden]`)) {
-                parentHidden = showMoreContent.closest(`[hidden]`);
-                parentHidden.hidden = false;
-            }
-            let originalHeight = showMoreContent.offsetHeight;
-            parentHidden ? (parentHidden.hidden = true) : null;
-            showMoreContent.style.height = `${hiddenHeight}px`;
-            return originalHeight;
-        }
-        function showMoreActions(e) {
-            const targetEvent = e.target;
-            const targetType = e.type;
-            if (targetType === "click") {
-                if (targetEvent.closest("[data-showmore-button]")) {
-                    const showMoreButton = targetEvent.closest("[data-showmore-button]");
-                    const showMoreBlock = showMoreButton.closest("[data-showmore]");
-                    const showMoreContent = showMoreBlock.querySelector("[data-showmore-content]");
-                    const showMoreSpeed = showMoreBlock.dataset.showmoreButton ? showMoreBlock.dataset.showmoreButton : "500";
-                    const hiddenHeight = getHeight(showMoreBlock, showMoreContent);
-                    if (!showMoreContent.classList.contains("_slide")) {
-                        showMoreBlock.classList.contains("_showmore-active")
-                            ? _slideUp(showMoreContent, showMoreSpeed, hiddenHeight)
-                            : _slideDown(showMoreContent, showMoreSpeed, hiddenHeight);
-                        showMoreBlock.classList.toggle("_showmore-active");
-                    }
-                }
-            } else if (targetType === "resize") {
-                showMoreBlocksRegular && showMoreBlocksRegular.length ? initItems(showMoreBlocksRegular) : null;
-                mdQueriesArray && mdQueriesArray.length ? initItemsMedia(mdQueriesArray) : null;
-            }
-        }
-    });
-}
 //================================================================================================================================================================================================================================================================================================================
 // Прочие полезные функции ================================================================================================================================================================================================================================================================================================================
 //================================================================================================================================================================================================================================================================================================================
@@ -598,14 +465,6 @@ export function FLS(message) {
             console.log(message);
         }
     }, 0);
-}
-// Получить цифры из строки
-export function getDigFromString(item) {
-    return parseInt(item.replace(/[^\d]/g, ""));
-}
-// Форматирование цифр типа 100 000 000
-export function getDigFormat(item) {
-    return item.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, "$1 ");
 }
 // Убрать класс из всех элементов массива
 export function removeClasses(array, className) {
--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -1,52 +1,50 @@
-/*
-Документация по работе в шаблоне: 
-Документация слайдера: https://swiperjs.com/
-Сниппет(HTML): swiper
-*/
-
-// Подключаем слайдер Swiper из node_modules
-// При необходимости подключаем дополнительные модули слайдера, указывая их в {} через запятую
-// Пример: { Navigation, Autoplay }
-import Swiper, { Navigation } from 'swiper';
-/*
-Основниые модули слайдера:
-Navigation, Pagination, Autoplay, 
-EffectFade, Lazy, Manipulation
-Подробнее смотри https://swiperjs.com/
-*/
-
-// Стили Swiper
-// Базовые стили
+import Swiper, { Navigation } from "swiper";
 import "../../scss/base/swiper.scss";
-// Полный набор стилей из scss/libs/swiper.scss
-// import "../../scss/libs/swiper.scss";
-// Полный набор стилей из node_modules
-// import 'swiper/css';
+
+//main-slider
+//========================================================================================================================================================
+// $(".main-page__slider-inner").slick({
+//     nextArrow: $(".main-page .main-page__nav_next"),
+//     prevArrow: $(".main-page .main-page__nav_prev"),
+//     zIndex: 10,
+//     speed: 800,
+//     fade: true,
+//     infinity: true,
+// });
+// $(".testi-page__slider").slick({
+//     nextArrow: $(".testi-page .testi-page__nav_next"),
+//     prevArrow: $(".testi-page .testi-page__nav_prev"),
+//     zIndex: 10,
+//     speed: 800,
+//     infinity: true,
+// });
 
 // Инициализация слайдеров
 function initSliders() {
-	// Перечень слайдеров
-	// Проверяем, есть ли слайдер на стронице
-	if (document.querySelector('.swiper')) { // Указываем скласс нужного слайдера
-		// Создаем слайдер
-		new Swiper('.swiper', { // Указываем скласс нужного слайдера
-			// Подключаем модули слайдера
-			// для конкретного случая
-			modules: [Navigation],
-			observer: true,
-			observeParents: true,
-			slidesPerView: 1,
-			spaceBetween: 0,
-			autoHeight: true,
-			speed: 800,
+    // Перечень слайдеров
+    // Проверяем, есть ли слайдер на стронице
+    if (document.querySelector(".swiper")) {
+        // Указываем скласс нужного слайдера
+        // Создаем слайдер
+        new Swiper(".swiper", {
+            // Указываем скласс нужного слайдера
+            // Подключаем модули слайдера
+            // для конкретного случая
+            modules: [Navigation],
+            observer: true,
+            observeParents: true,
+            slidesPerView: 1,
+            spaceBetween: 0,
+            autoHeight: true,
+            speed: 800,
 
-			//touchRatio: 0,
-			//simulateTouch: false,
-			//loop: true,
-			//preloadImages: false,
-			//lazy: true,
+            //touchRatio: 0,
+            //simulateTouch: false,
+            //loop: true,
+            //preloadImages: false,
+            //lazy: true,
 
-			/*
+            /*
 			// Эффекты
 			effect: 'fade',
 			autoplay: {
@@ -55,30 +53,30 @@ function initSliders() {
 			},
 			*/
 
-			// Пагинация
-			/*
+            // Пагинация
+            /*
 			pagination: {
 				el: '.swiper-pagination',
 				clickable: true,
 			},
 			*/
 
-			// Скроллбар
-			/*
+            // Скроллбар
+            /*
 			scrollbar: {
 				el: '.swiper-scrollbar',
 				draggable: true,
 			},
 			*/
 
-			// Кнопки "влево/вправо"
-			navigation: {
-				prevEl: '.swiper-button-prev',
-				nextEl: '.swiper-button-next',
-			},
+            // Кнопки "влево/вправо"
+            navigation: {
+                prevEl: ".swiper-button-prev",
+                nextEl: ".swiper-button-next",
+            },
 
-			// Брейкпоинты
-			/*
+            // Брейкпоинты
+            /*
 			breakpoints: {
 				320: {
 					slidesPerView: 1,
@@ -99,45 +97,12 @@ function initSliders() {
 				},
 			},
 			*/
-			// События
-			on: {
-
-			}
-		});
-	}
-}
-// Скролл на базе слайдера (по классу swiper_scroll для оболочки слайдера)
-function initSlidersScroll() {
-	let sliderScrollItems = document.querySelectorAll('.swiper_scroll');
-	if (sliderScrollItems.length > 0) {
-		for (let index = 0; index < sliderScrollItems.length; index++) {
-			const sliderScrollItem = sliderScrollItems[index];
-			const sliderScrollBar = sliderScrollItem.querySelector('.swiper-scrollbar');
-			const sliderScroll = new Swiper(sliderScrollItem, {
-				observer: true,
-				observeParents: true,
-				direction: 'vertical',
-				slidesPerView: 'auto',
-				freeMode: {
-					enabled: true,
-				},
-				scrollbar: {
-					el: sliderScrollBar,
-					draggable: true,
-					snapOnRelease: false
-				},
-				mousewheel: {
-					releaseOnEdges: true,
-				},
-			});
-			sliderScroll.scrollbar.updateSize();
-		}
-	}
+            // События
+            on: {},
+        });
+    }
 }
 
 window.addEventListener("load", function (e) {
-	// Запуск инициализации слайдеров
-	initSliders();
-	// Запуск инициализации скролла на базе слайдера (по классу swiper_scroll)
-	//initSlidersScroll();
+    initSliders();
 });
