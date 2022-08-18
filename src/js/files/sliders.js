6c42b858a992fc0416832385e02885067cec7fc1
Sergey Petrinich <petrinichshadow@gmail.com>
2022-08-18 16:34:16 +0300

    pet project updateted to v3 of FLS template

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -1,32 +1,14 @@
 import Swiper, { Navigation } from "swiper";
 import "../../scss/base/swiper.scss";
 
-//main-slider
-//========================================================================================================================================================
-// $(".main-page__slider-inner").slick({
-//     nextArrow: $(".main-page .main-page__nav_next"),
-//     prevArrow: $(".main-page .main-page__nav_prev"),
-//     zIndex: 10,
-//     speed: 800,
-//     fade: true,
-//     infinity: true,
-// });
-// $(".testi-page__slider").slick({
-//     nextArrow: $(".testi-page .testi-page__nav_next"),
-//     prevArrow: $(".testi-page .testi-page__nav_prev"),
-//     zIndex: 10,
-//     speed: 800,
-//     infinity: true,
-// });
-
 // Инициализация слайдеров
 function initSliders() {
     // Перечень слайдеров
     // Проверяем, есть ли слайдер на стронице
-    if (document.querySelector(".swiper")) {
+    if (document.querySelector(".main__swiper")) {
         // Указываем скласс нужного слайдера
         // Создаем слайдер
-        new Swiper(".swiper", {
+        new Swiper(".main__swiper", {
             // Указываем скласс нужного слайдера
             // Подключаем модули слайдера
             // для конкретного случая
@@ -35,70 +17,41 @@ function initSliders() {
             observeParents: true,
             slidesPerView: 1,
             spaceBetween: 0,
-            autoHeight: true,
+            autoHeight: false,
             speed: 800,
 
-            //touchRatio: 0,
-            //simulateTouch: false,
-            //loop: true,
-            //preloadImages: false,
-            //lazy: true,
-
-            /*
-			// Эффекты
-			effect: 'fade',
-			autoplay: {
-				delay: 3000,
-				disableOnInteraction: false,
-			},
-			*/
-
-            // Пагинация
-            /*
-			pagination: {
-				el: '.swiper-pagination',
-				clickable: true,
-			},
-			*/
-
-            // Скроллбар
-            /*
-			scrollbar: {
-				el: '.swiper-scrollbar',
-				draggable: true,
-			},
-			*/
+            loop: true,
 
             // Кнопки "влево/вправо"
             navigation: {
-                prevEl: ".swiper-button-prev",
-                nextEl: ".swiper-button-next",
+                prevEl: ".main .main__nav_prev",
+                nextEl: ".main .main__nav_next",
             },
+        });
+    }
 
-            // Брейкпоинты
-            /*
-			breakpoints: {
-				320: {
-					slidesPerView: 1,
-					spaceBetween: 0,
-					autoHeight: true,
-				},
-				768: {
-					slidesPerView: 2,
-					spaceBetween: 20,
-				},
-				992: {
-					slidesPerView: 3,
-					spaceBetween: 20,
-				},
-				1268: {
-					slidesPerView: 4,
-					spaceBetween: 30,
-				},
-			},
-			*/
-            // События
-            on: {},
+    if (document.querySelector(".testi__slider")) {
+        // Указываем скласс нужного слайдера
+        // Создаем слайдер
+        new Swiper(".testi__slider", {
+            // Указываем скласс нужного слайдера
+            // Подключаем модули слайдера
+            // для конкретного случая
+            modules: [Navigation],
+            observer: true,
+            observeParents: true,
+            slidesPerView: 1,
+            spaceBetween: 0,
+            autoHeight: false,
+            speed: 800,
+
+            loop: true,
+
+            // Кнопки "влево/вправо"
+            navigation: {
+                prevEl: ".testi .testi__nav_prev",
+                nextEl: ".testi .testi__nav_next",
+            },
         });
     }
 }
